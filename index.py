69a6c06957f3a7954769c26f1c10139b5a573903
ma4ont <165573536+ma4ont@users.noreply.github.com>
2024-04-08 21:05:48 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Зависимость</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <iframe src="https://www.meteoblue.com/en/weather/widget/daily/ip_romania_675514?geoloc=fixed&days=7&tempunit=CELSIUS&windunit=METER_PER_SECOND&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&uv=0&humidity=0&precipitation=0&precipitationprobability=0&spot=0&pressure=0&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 378px; height: 420px"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/en/weather/week/ip_romania_675514?utm_source=weather_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">meteoblue</a></div>
+    <h1>Боротся с зависимостью можно по разному на пример ставить ограничения</h1>
+    <p>но есть ещё куча способов</p>
+    <h2>и они разные. А плоха Зависимость тем что люди тупеют и тратять время в пустую а могли развиватя</h2>
+             <ul>
+                   <li>надеюсь информация была полезна</li>
+                   <li>!!!</li>
+             </ul>
+     <img src="https://static.tildacdn.com/tild6635-6331-4862-b132-366663663165/scale_1200.jpg" alt="Image 1">
+</body>
+</html>
